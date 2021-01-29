b4ce4bb273f8823ba814e870e2828addcf15cb59
Matt Kraatz <matt.kraatz@gmail.com>
2021-01-28 18:25:58 -0600

    basic movement and sock collection

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -3,6 +3,7 @@ import { IImageConstructor } from '../interfaces/image.interface';
 export class Player extends Phaser.GameObjects.Image {
   private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
   private walkingSpeed: number;
+  private diagonalWalkingSpeed: number;
 
   constructor(aParams: IImageConstructor) {
     super(aParams.scene, aParams.x, aParams.y, aParams.texture);
@@ -16,6 +17,7 @@ export class Player extends Phaser.GameObjects.Image {
 
   private initVariables(): void {
     this.walkingSpeed = 5;
+    this.diagonalWalkingSpeed = 4;
   }
 
   private initImage(): void {
@@ -31,16 +33,34 @@ export class Player extends Phaser.GameObjects.Image {
   }
 
   private handleInput(): void {
-    if (this.cursors.right.isDown) {
+    if (this.cursors.right.isDown && this.cursors.up.isDown) {
+      this.x += this.diagonalWalkingSpeed;
+      this.y -= this.diagonalWalkingSpeed;
+      this.setAngle(315);
+    } else if (this.cursors.right.isDown && this.cursors.down.isDown) {
+      this.x += this.diagonalWalkingSpeed;
+      this.y += this.diagonalWalkingSpeed;
+      this.setAngle(45);
+    } else if (this.cursors.right.isDown) {
       this.x += this.walkingSpeed;
-      this.setFlipX(false);
+      this.setAngle(0);
+    } else if (this.cursors.left.isDown && this.cursors.up.isDown) {
+      this.x -= this.diagonalWalkingSpeed;
+      this.y -= this.diagonalWalkingSpeed;
+      this.setAngle(225);
+    } else if (this.cursors.left.isDown && this.cursors.down.isDown) {
+      this.x -= this.diagonalWalkingSpeed;
+      this.y += this.diagonalWalkingSpeed;
+      this.setAngle(135);
     } else if (this.cursors.left.isDown) {
       this.x -= this.walkingSpeed;
-      this.setFlipX(true);
+      this.setAngle(180);
     } else if (this.cursors.up.isDown) {
       this.y -= this.walkingSpeed;
+      this.setAngle(270);
     } else if (this.cursors.down.isDown) {
       this.y += this.walkingSpeed;
+      this.setAngle(90);
     }
   }
 }
