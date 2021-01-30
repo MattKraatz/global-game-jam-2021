159604c27fce6562bc4f90c4f57ea1bc157c5f4a
Matt Kraatz <matt.kraatz@gmail.com>
2021-01-30 11:07:42 -0600

    start screen

--- src/scenes/start-scene.ts
+++ src/scenes/start-scene.ts
@@ -7,7 +7,14 @@ export class StartScene extends Phaser.Scene {
 		super({
 			key: 'StartScene'
 		});
-		
+	}
+
+	create(): void {
+		const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+		const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+		this.add.text(screenCenterX, screenCenterY - 20, 'SNEAKERHEADS', { fontSize: '32px' }).setOrigin(0.5);
+		this.add.text(screenCenterX, screenCenterY + 10, 'Press any key to start').setOrigin(0.5);
+
 		// press any key to start
 		this.input.keyboard.on('keydown', () => this.scene.start('BootScene'));
 	}
